44394c37fa489b2f3cdbef8264bad75c33f16aa6
Jeffery Patterson <jpatterson933@ucla.edu>
2023-03-27 14:50:55 -0700

    1. Fixed errors

--- src/views/landing.js
+++ src/views/landing.js
@@ -1,13 +1,12 @@
 import React from "react";
 import './index.css';
-import { AsideContainer, HeaderContainer } from "./containers";
+import { HeaderContainer } from "./containers";
 
 export const LandingPage = () => {
     return (
         <>
         {/* // we are doing flex with the direction being a row for the header elements */}
             <HeaderContainer />
-            {/* <AsideContainer /> */}
         </>
     )
 }
